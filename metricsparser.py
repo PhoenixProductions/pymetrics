625d3db1530deb6a4268e584510760eaf06d077f
Michael Hughes <michael@phoenixproductions.org.uk>
2012-04-17 22:19:53 +0100

    worked out grammar better

--- metricsparser.py
+++ metricsparser.py
@@ -2,48 +2,57 @@ from parser import Parser
 
 class MetricsParser(Parser):
  tokens = (
-  'WORD', 'AT', 'NUMBER', 'FLOAT', 'COLON'
+  'WORD', 'AT', 'NUMBER', 'FLOAT', 'COLON', 'SPACE'
  )
 
  t_FLOAT   = r'((\d*\.\d+)(E[\+-]?\d+)?|([1-9]\d*E[\+-]?\d+))'
  t_NUMBER = r'\d+' 
  t_COLON = r':'
-
- def t_WORD(self,t):
-  r'\W+'
-  print 'matched word'
+ t_WORD = r'[a-zA-Z_]+'
+ t_AT = r'at|AT|aT|At'
+ 
+# def t_WORD(self,t):
+#  r'\W+'
  
  def t_newline(self,t):
   r'\n+'
 
  def t_error(self,t):
+  print "Illegal Character '%s'" % t.value[0]
   t.lexer.skip(1)
 
  def p_command(self,p):
-  '''command : 
-             | action'''
-  print "Command " 
-  print p[0]
- 
+  """ command : action
+              | action time
+  """
+  lp = len(p)
+
+
+ def p_time(self,p):
+  """ time : AT NUMBER 
+  """
+  print "Matched time"
+  
  def p_action(self,p):
-  '''action : WORD'''
-  l = len(p)
-  if l == 1:
-   print 'simply unit'
-  elif l == 2:
-   print 'quantified unit'
-  else:
-   print 'unknown command'
+  """action  : simpleaction
+             | actionwithcategory
+  """
+  print "Matched action {}".format(len(p))
+
+ def p_simpleaction(self,p):
+  """simpleaction : WORD
+  """
+  print "Simple action {}".format(len(p))
+
+ def p_actionwithcategory(self,p):
+  """actionwithcategory : WORD WORD
+  """
+  print "Matched actionwith category {}".format(len(p))
 
  
-# def p_time(p):
-#  '''time : NUMBER COLON NUMBER'''
-#  print "time"
-#  print p
-
  def p_error(self,p):
   if p:
-   print "parse error %s", p.value
+   print "parse error %s" % p.value
   else:
    print "error at EOF"
 if __name__ == '__main__':
